f7a5b52da3a2d5f791a2548fb85a9c569dc172ce
Korotkova Julia <korotkovayuliav@gmail.com>
2022-09-13 12:08:29 +0200

    done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char symbolC = 99;
+        char symbolH = 104;
+        char symbolA = 97;
+        char symbolR = 114;
+        System.out.println(symbolC + symbolH + symbolA +symbolR);
     }
 }
